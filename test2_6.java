a64a5bfd66ab8c9cbedc6d45964cd942bfbacd97
isuHan <116141262+isuHan@users.noreply.github.com>
2023-09-06 23:47:58 +0900

    commit

--- test2_6.java
+++ test2_6.java
@@ -2,39 +2,5 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class test2_6 {
-    public boolean isPrime(int num) {
-        if(num == 1) return false;
-        for(int i=2; i<num; i++) {
-            if(num % i == 0) return false;
-        }
-        return true;
-    }
 
-    public ArrayList<Integer> solution(int n, int[] arr) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        for(int i=0; i<n; i++) {
-            int tmp = arr[i];
-            int res = 0;
-            while(tmp > 0) {
-                int t = tmp % 10;
-                res = res * 10 + t;
-                tmp = tmp / 10;
-            }
-        }
-
-        return answer;
-    }
-
-    public static void main(String[] args) {
-        test2_6 T = new test2_6();
-        Scanner kb = new Scanner(System.in);
-        int n = kb.nextInt();
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++) {
-            arr[i] = kb.nextInt();
-        }
-        for (int x : T.solution(n, arr)) {
-            System.out.println(x + " ");
-        }
-    }
 }
